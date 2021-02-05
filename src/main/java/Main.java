2f65a56a129e145b85f6b3b9d0ce233111dd511f
Désormeaux, Simon <desormeaux.simon@courrier.uqam.ca>
2021-02-05 15:04:29 -0500

    FIX - dedoublement de la fonction main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.io.File;
 
 public class Main {
-<<<<<<< src/main/java/Main.java
+
 
     private static final String JSON_REFERENCE_PATH = "src/main/java/claimsReference.json";
     public static Customer customerInfo;
@@ -11,20 +11,10 @@ public class Main {
         initializeObjects(args[0],JSON_REFERENCE_PATH);
     }
 
-    public static void initializeObjects(String commandLineArgumentPath, String referencePath){
+    public static void initializeObjects(String commandLineArgumentPath, String referencePath) {
         File firstCliArgument = new File(commandLineArgumentPath);
         customerInfo = JacksonUtils.JsonToCustomerInput(firstCliArgument);
         File referenceFile = new File(referencePath);
         referenceObject = JacksonUtils.JsonToReference(referenceFile);
-=======
-    public static void main(String[] args) {
-
-        //TODO mettre sa dans une methode
-        File inputFile = JacksonUtils.transformToFile(args[0]);
-        Customer customerInfo = JacksonUtils.JsonToObjectInput(inputFile);
-        File referenceFile = JacksonUtils.transformToFile("src/main/java/claimsReference.json");
-        CareReference referenceObject = JacksonUtils.JsonToReference(referenceFile);
-
->>>>>>> src/main/java/Main.java
     }
 }
