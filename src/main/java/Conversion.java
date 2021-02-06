4f3b3ff07c5ffe8c0d6515e73e655b54f881d2c7
Mathieu Lalonde <moi@mathieulalonde.com>
2021-02-06 18:43:11 -0500

    FIX - Correction d'un bug de conversion et simplification du getTreatmentCostFloat

--- src/main/java/Conversion.java
+++ src/main/java/Conversion.java
@@ -3,7 +3,6 @@ public class Conversion {
     public static float convertCostStringToFloat(String amount){
         amount = replaceCommasByPeriods(amount);
         amount = removeAllButNumbersAndPeriods(amount);
-        amount = removeAllButLastPeriod(amount);
         return Float.parseFloat(amount);
     }
 
@@ -12,20 +11,6 @@ public class Conversion {
     }
 
     public static String removeAllButNumbersAndPeriods(String value){
-        return value.replaceAll("[^.\\d]","");
-    }
-
-    public static String removeAllButLastPeriod(String value){
-        String newValue = "";
-
-        String[] result = value.split("\\.");
-        for (int i=0; i < result.length; i++) {
-            if (i < result.length -1 ){
-                newValue = newValue + result[i];
-            } else {
-                newValue = newValue + "." + result[i];
-            }
-        }
-        return newValue;
+        return value.replaceAll("\\.(?![^.]+$)|[^0-9.]","");
     }
 }
