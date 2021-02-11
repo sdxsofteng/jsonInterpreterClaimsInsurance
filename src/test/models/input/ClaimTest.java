5aca230dd92a4c17f28edc9d4d967ace1b2b9c91
David <david.genois13@gmail.com>
2021-02-11 01:53:56 -0500

    AJT - Classe de tests pour model/input/Claim

---
+++ src/test/models/input/ClaimTest.java
@@ -0,0 +1,35 @@
+package models.input;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ClaimTest {
+
+    @ParameterizedTest
+    @MethodSource("claimsSource")
+    @DisplayName("Claim getTreatmentCostFloat method should return the expected float value for its string counterpart")
+    public void testClaimGetCostAsFloat(Claim claim) {
+        String strippedCostString = claim.getTreatmentCost().trim().replace("$", "");
+        String formattedCostString = strippedCostString.replace(",", ".");
+        float expectedValue = Float.parseFloat(formattedCostString);
+        assertEquals(expectedValue, claim.getTreatmentCostFloat());
+    }
+
+    /**
+     * Liste d'arguments pour les tests qui nécéssites des claims
+     */
+    static Stream<Arguments> claimsSource() {
+        return Stream.of(
+                Arguments.of(new Claim(200, "2021-01-11", "0$")),
+                Arguments.of(new Claim(100, "2021-01-01", "100$")),
+                Arguments.of(new Claim(313,"2021-01-31", "1000000.00$")),
+                Arguments.of(new Claim(400,"2021-02-31", "1000000,00$"))
+        );
+    }
+}
