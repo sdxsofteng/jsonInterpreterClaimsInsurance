e40dbd295e82ce77352dcee403f2e3fb9df627b2
Mathieu Lalonde <moi@mathieulalonde.com>
2021-04-13 12:48:00 -0400

    AJT - Cas de tests

--- src/main/java/models/output/Message.java
+++ src/main/java/models/output/Message.java
@@ -7,7 +7,7 @@ public enum Message {
     MISSING_ARGUMENTS(-2, "Paramètres d'execution absents"),
     INCORRECT_ARGUMENTS(-3, "Paramètres d'execution invalides"),
 
-    INVALID_INPUT_FILE(-4, "Fichier d'entee invalide"),
+    INVALID_INPUT_FILE(-4, "Fichier d'entree invalide"),
     INVALID_OUTPUT_FILE(-5, "Fichier sortie invalide"),
 
     INVALID_REFERENCE_FILE(-6, "Données internes invalides"),
@@ -15,11 +15,11 @@ public enum Message {
     ANALYTICS_FILE_READ_FAILURE(-8, "Incapable de lire le fichier statistique"),
     ANALYTICS_FILE_WRITE_FAILURE(-9, "Incapable de mettre à jour le fichier statistique"),
 
-    MISSING_FILENUMBER( -10, "Numéro de dossier inexistant"),
-    INCORRECT_FILENUMBER( -11, "Numéro de dossier incorrect"),
+    MISSING_FILENUMBER( -10, "Numéro de dossier manquant"),
+    INCORRECT_FILENUMBER( -11, "Numéro de dossier invalide"),
 
-    MISSING_INVOICE_DATE( -12, "Mois de reclamation inexistant"),
-    INCORRECT_INVOICE_DATE( -13, "Mois de reclamation incorrect"),
+    MISSING_INVOICE_DATE( -12, "Mois de reclamation manquant"),
+    INCORRECT_INVOICE_DATE( -13, "Mois de reclamation invalide"),
 
     MISSING_CLAIMS( -14, "Reclamation inexistante"),
 
@@ -31,10 +31,10 @@ public enum Message {
 
     MISSING_TREATMENT_COST( -24, "Montant de soin manquant"),
     INVALID_TREATMENT_COST( -25, "Montant de soin invalide"),
-    INVALID_TREATMENT_COST_TOO_LOW( -26, "Montant de soin ne peut pas etre egal ou plus bas que 0"),
-    MONTHLY_TREATMENT_COST_TOO_HIGH( -27, "Montant mensuel pour ce soin dépasse le maximum de 500$"),
+    INVALID_TREATMENT_COST_TOO_LOW( -26, "Le cout d'un soin ne peut pas etre egal ou inferieur a 0"),
+    MONTHLY_TREATMENT_COST_TOO_HIGH( -27, "Le montant mensuel total pour ce soin dépasse le maximum de 500$"),
 
-    CLAIM_DATE_LIMIT_REACHED( -30, "Maximum de soins par jour dépasses"),
+    CLAIM_DATE_LIMIT_REACHED( -30, "Maximum de soins par jour depasse"),
 
     OVER_MAX_INTEGER( -99, "Montant de reclamation trop eleve"),
     ;
